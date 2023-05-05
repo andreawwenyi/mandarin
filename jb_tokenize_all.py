41ff60cf0b7318e86bbe853fbedce4bb88e98546
andrea-w-wang <ayw255@nyu.edu>
2023-05-04 21:34:51 -0400

    fix: output filename

--- jb_tokenize_all.py
+++ jb_tokenize_all.py
@@ -6,6 +6,6 @@ import jb_tokenizer
 for p in Path("data/ptt-gossiping").glob("*.jsonl"):
     print(p)
     output_filename = str(p).replace("data", "data-jb-tokenized")
-    with jsonlines.open(f'{output_filename}.jsonl', mode='w') as writer:
+    with jsonlines.open(output_filename, mode='w') as writer:
         for line in jb_tokenizer.tokenize_one_jsonl(p):
             writer.write(line)
